28e014c2dd32b69f0f4e69df6c8c84482485bc9f
Maxim <maxbilenkin@gmail.com>
2023-04-30 23:09:59 -0400

    Part-1

---
+++ src/test/java/com/cydeo/ticketingorm2/TicketingOrm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingorm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingOrm2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
